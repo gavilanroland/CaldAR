2346e4f6fe2e90a610f823dbc2ff69325feb321c
gavilanroland <gavilanrolando@protonmail.com>
2020-12-01 16:22:20 -0300

    The boilers Database finalized

---
+++ src/controllers/boilers.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const items = require(".././routes/boilers")
+
+const app = express();
+
+//bodyparser Midelware
+
+app.use(bodyParser.json());
+
+//DB Config
+const db = require("../config/keys").mongoURI;
+
+//Connect with mongo DB
+mongoose
+    .connect(db)
+    .then(()=> console.log("Mongo DB Conected"))
+    .catch(err=> console.log(err));
+
+//use Routes
+app.use("./models/boilers", items);
+
+//PORT
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
